02b80c5d69bade15717ba153e8db5d95fdc9b17b
Amandeep <singhamandeep71@gmail.com>
2023-04-03 15:44:24 +0200

    added gif support, metadata function, config

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import itertools
 from PIL import Image
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import imageio
+import metadata
+import sys
 
-config = open("config.json")
+config = json.load(open("config.json"))
 
-data = json.load(config)
-height, width = data['size']['height'], data['size']['width']
+MAX_WORKER = config['max_worker']
+
+height, width = config['size']['height'], config['size']['width']
 
 
 def layerSetup(layerOrder: list[str]) -> list[list[str]]:
@@ -26,30 +30,52 @@ def getUniqueCombinations(layers: list[str]) -> list[list[str]]:
     return list(itertools.product(*layers))
 
 
-def genrateToken(layerSet, i):
+def generateToken(layerSet, i):
     print("Minting token ", i)
     token = Image.new("RGBA", (height, width), (0, 0, 0, 0))
 
     for layer in layerSet:
         img = Image.open(layer).convert("RGBA").resize((height, width))
         token.paste(img, (0, 0), img)
-    token.save("./build/NFT" + str(i) + ".png")
+    metadata.genrate_metadata(i, layerSet)
+    token.save("./build/tokens/NFT_" + str(i) + ".png")
+
+
+def generateGIFToken(layerSet, i):
+    print("Minting token ", i)
+    images = []
+
+    for layer in layerSet:
+        img = Image.open(layer).convert("RGBA").resize((height, width))
+        images.append(img)
+
+    imageio.mimsave("./build/tokens/NFT_" + str(i) + ".gif", images, duration=config['durationPerImage'])
 
 
 async def createNFT(layerCombinations, tokenQty=None):
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=MAX_WORKER) as executor:
         loop = asyncio.get_event_loop()
         tasks = []
         i = 1
-        for x in layerCombinations:
-            task = loop.run_in_executor(executor, genrateToken, x, i)
-            tasks.append(task)
-            i += 1
+        if(config['tokenType'] == 'img'):
+
+            for x in layerCombinations:
+                task = loop.run_in_executor(executor, generateToken, x, i)
+                tasks.append(task)
+                i += 1
+        elif(config['tokenType'] == 'gif'):
+            for x in layerCombinations:
+                task = loop.run_in_executor(executor, generateGIFToken, x, i)
+                tasks.append(task)
+                i += 1
+        else:
+            print('Invalid Config')
+            sys.exit()
         await asyncio.gather(*tasks)
 
 
 def genrate():
-    layers = layerSetup(data['layerOrder'])
+    layers = layerSetup(config['layerOrder'])
     loop = asyncio.get_event_loop()
     loop.run_until_complete(createNFT(layers))
 
---
+++ metadata.py
@@ -0,0 +1,34 @@
+import json
+from datetime import date
+import hashlib
+
+
+config = json.load(open('config.json'))
+template = json.load(open('template.json'))
+
+project_name = config['projectName']
+description = config['description']
+layer_order = config['layerOrder']
+date_var = date.today()
+
+def genrate_metadata(edition, attributes):
+    template['name'] = project_name + " : " + str(edition)
+    template['description'] = description
+    template['edition'] = edition
+    template['date'] = str(date_var)
+    template['sha1'] = genrate_hash(''.join(attributes))
+    attributes_values = []
+
+    for i in range(len(layer_order)):
+        attributes_values.append({
+            "trait_types": layer_order[i],
+            "value": attributes[i]
+        })
+    template['attributes'] = attributes_values
+
+    with open("./build/metadata/NFT_" + str(edition) +"_metadata.json", "w") as outfile:
+        json.dump(template, outfile)
+
+def genrate_hash(attributes):
+    hash_object = hashlib.sha1(attributes.encode('ascii'))
+    return hash_object.hexdigest()
